df046c244822677abbb05f3b756c81369fe3d127
AbhipsaThakur <abhipsathaku933@gmail.com>
2026-01-04 15:22:30 +0530

    Adding proper file path

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -23,7 +23,7 @@ const Contact = () => {
   e.preventDefault();
 
   try {
-    const response = await fetch('http://localhost:5000/api/contact', {
+    const response = await fetch('https://portfolio-backend-vercel-7nxb.vercel.app/api/contact', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
